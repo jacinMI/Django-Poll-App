a34b85f2d3ca5b79a9d7309f4198becf6def0acb
Precious Akpan <67706629+preciousakpan@users.noreply.github.com>
2023-03-20 14:11:50 +0100

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'pollme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poll',
+        'USER': 'aitech',
+        'PASSWORD': 'admin@12345',
+        'HOST': "tester.mysql.database.azure.com",
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
